8580b411a554f0e03173b45181b7e6cbed2af880
emxcoder <emx.coder@gmail.com>
2026-03-29 22:40:05 +0200

    Update for get style css

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
+from urllib import request
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from pathlib import Path
 
 app = FastAPI()
+app.mount('/page/static', StaticFiles(directory='page/static'),name='static')
+
+templates = Jinja2Templates(directory='page/templates')
 
 HTML_FILE = Path('page/index.html')
 
 @app.get('/',response_class=HTMLResponse)
-def home():
-    return HTML_FILE.read_text(encoding='utf-8')
+async def home():
+    return templates.TemplateResponse('index.html',{'request':request,'title':'My Page'})
 
 @app.get('/products')
 async def product():
