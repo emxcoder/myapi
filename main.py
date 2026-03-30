09783cf0d3ceb5cbff866f85ecd516f3ad5e7693
emxcoder <emx.coder@gmail.com>
2026-03-30 21:02:25 +0200

    ready

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 
-from fastapi import FastAPI, Request
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, Request, UploadFile, File
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.middleware.cors import CORSMiddleware
 from rembg import remove
-
-
+from fastapi.responses import Response
+import datetime
 
 app = FastAPI()
 
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # ou teu domínio do vercel
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_origins=['*'],
+    allow_methods=['post'],
+    allow_headers=['*'],
 )
+
 
 # Config
 app.mount('/static', StaticFiles(directory='static'),name='static')
@@ -24,19 +24,23 @@ templates = Jinja2Templates(directory='templates')
 
 # HTML_FILE = Path('templates/index.html')
 
+
 @app.get('/')
 async def home(request: Request):
+    timeToday = datetime.datetime.now().year
     return templates.TemplateResponse(
         request,
         'index.html',
         {
             'name':'EmxCoder',
+            'year':timeToday
            
         }
     )
 
-# @app.post('/remove-bg')
-# async def removeBG(file: UploadFile= File(...)):
-#     input_data = await file.read()
-#     output = remove(input_data)
-#     return Response(content=output,media_type='image/png')
+
+@app.post('/removeBg')
+async def removeBG(file:UploadFile=File(...)):
+    input_data = await file.read()
+    output = remove(input_data)
+    return Response(content=output, media_type='image/png')
--- static/script.js
+++ static/script.js
@@ -1,7 +1,8 @@
 
 
-document.getElementById('year').textContent = new Date().getFullYear();
+// document.getElementById('year').textContent = new Date().getFullYear();
 document.getElementById('vbu').addEventListener('click', clicked)
+
 
 async function clicked() {
     document.getElementById('nome').innerText = 'EMXCODER'
