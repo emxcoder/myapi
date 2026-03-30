d3c1c95fe9d83f8af54aff5e092462e25bb3084a
emxcoder <emx.coder@gmail.com>
2026-03-30 20:09:32 +0200

    create backend to render for api removebg

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 
 from fastapi import FastAPI, Request
-from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from rembg import remove
 
 
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # ou teu domínio do vercel
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Config
 app.mount('/static', StaticFiles(directory='static'),name='static')
@@ -26,19 +35,8 @@ async def home(request: Request):
         }
     )
 
-@app.get('/products')
-async def product():
-    productsList = ['Iphone','Ipad','Ipod']
-    return productsList
-
-class Model(BaseModel):
-    identifier:str = 'base'
-
-@app.get('/devices')
-async def devices(identifier:str):
-    return identifier
-
-@app.get('/image')
-def images():
-    
-    print('Clicked')
+# @app.post('/remove-bg')
+# async def removeBG(file: UploadFile= File(...)):
+#     input_data = await file.read()
+#     output = remove(input_data)
+#     return Response(content=output,media_type='image/png')
--- static/script.js
+++ static/script.js
@@ -4,11 +4,6 @@ document.getElementById('year').textContent = new Date().getFullYear();
 document.getElementById('vbu').addEventListener('click', clicked)
 
 async function clicked() {
-    const res = await fetch('/products', {
-        method: 'GET'
-    })
-    const data = await res.json()
-
     document.getElementById('nome').innerText = 'EMXCODER'
 }
 
