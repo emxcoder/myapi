cb10983ebf01bb4974cbcee5b60f3e4a7659962b
emxcoder <emx.coder@gmail.com>
2026-03-31 04:17:04 +0200

    full fixes and routes

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 
-from fastapi import FastAPI, Request, UploadFile, File
+import io
+from fastapi import FastAPI, File, Request, UploadFile
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.middleware.cors import CORSMiddleware
-from rembg import remove
-from fastapi.responses import Response
-import datetime
-import os
-os.environ["RMBG_NO_GPU"] = "1"
+from rembg import remove, new_session
 
+import datetime
+from PIL import Image
+import os
 
 app = FastAPI()
 
@@ -24,6 +24,9 @@ app.add_middleware(
 app.mount('/static', StaticFiles(directory='static'),name='static')
 templates = Jinja2Templates(directory='templates')
 
+os.makedirs('uploads',exist_ok=True)
+app.mount('/uploads',StaticFiles(directory='uploads'),name='uploads')
+
 
 # HTML_FILE = Path('templates/index.html')
 
@@ -34,6 +37,7 @@ async def home(request: Request):
     return templates.TemplateResponse(
         request,
         'index.html',
+
         {
             'name':'EmxCoder',
             'year':timeToday
@@ -41,9 +45,45 @@ async def home(request: Request):
         }
     )
 
+@app.get('/about')
+async def about(request: Request):
+    return templates.TemplateResponse(
+        request,
+        'about/about.html',
+        {'date':datetime.datetime.now().year}
+    )
 
 @app.post('/removeBg')
-async def removeBG(file:UploadFile=File(...)):
-    input_data = await file.read()
-    output = remove(input_data)
-    return Response(content=output, media_type='image/png')
+async def removeBGAction(request:Request, file:UploadFile=File(...)):
+
+    session = new_session()
+    contents = await file.read()
+
+    input_image = Image.open(io.BytesIO(contents)).convert("RGBA")
+    output_image = await remove(input_image,session=session)
+
+
+    output_path = "uploads/output.png"
+    try:
+        output_image.save(output_path)
+    except:
+        print('erro')
+    
+    
+
+    return templates.TemplateResponse(
+        request,
+        "remover/removebg.html",
+        {
+            "image": "/uploads/output.png"
+        }
+    )
+
+
+@app.get('/remove')
+async def removepage(request: Request):
+    return templates.TemplateResponse(
+        request,
+        'remover/removebg.html',
+        {'date':datetime.datetime.now().year}
+    )
